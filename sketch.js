c3311cf1ec866b33eb79213bf354fef6228d5ae5
Soledad <soledad.arayau@pucv.cl>
2020-11-20 14:24:08 -0300

    edit

--- sketch.js
+++ sketch.js
@@ -1,54 +1,66 @@
 // Simetría correspondiente al número de reflejos.
 // Cambia el valor para obtener un número diferente de reflexiones
+
+
 let symmetry = 9;
 
 let angle = 360 / symmetry;
-let saveButton, clearButton, mouseButton, keyboardButton, amarilloButton, rojoButton, azulButton, circulosButton; 
+let saveButton, clearButton, mouseButton, keyboardbutton, amarilloButton, rojoButton, azulButton, circulosButton;
 let slider;
-let sliderR,butSliderR;
-let sliderG,butSliderG;
-let sliderB,butSliderB;
-let circulo=false;
+let sliderR, butSliderR;
+let sliderG, butSliderG;
+let sliderB, butSliderB;
+let sliderAlpha, butSliderAlpha;
+let circulo = false;
 
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(940, 700);
   angleMode(DEGREES);
   background(0);
 
   // Creando el botón para guardar para el archivo
-  saveButton = createButton('save');
-  saveButton.mousePressed(saveFile);
+  saveButton = document.getElementById("save");
+  // saveButton.mousePressed(saveFile);
 
   // Creando el botón para borrar la pantalla
-  clearButton = createButton('clear');
-  clearButton.mousePressed(clearScreen);
+  clearButton = document.getElementById("clear");
+  // clearButton.mousePressed(clearScreen);
 
   // Creando el botón para la pantalla completa
-  fullscreenButton = createButton('Full Screen');
-  fullscreenButton.mousePressed(screenFull);
-  
-   //Configurando boton circulos
-  circulosButton = createButton("Circulos");
-  circulosButton.mousePressed(setCirculos);
-  
-  circulosButton = createButton("resetCirculos");
-  circulosButton.mousePressed(resetCirculos);
-  
+  fullscreenButton = document.getElementById("fullscreen");
+  // fullscreenButton.mousePressed(screenFull);
+
+  //Configurando boton circulos
+  circulosButton = document.getElementById("setcirculo");
+  // circulosButton.mousePressed(setCirculos);
+
+  circulosButton = document.getElementById("resetcirculo");
+  // circulosButton.mousePressed(resetCirculos);
+
   // Configurando el deslizador para el grosor del pincel
-  brushSizeSlider = createButton('Brush Size Slider');
-  sizeSlider = createSlider(1, 32, 4, 0.1);
-  
+  // brushSizeSlider = createButton('Brush Size Slider');
+  sizeSlider = document.getElementById('slider');
+
   // Escala de color en R
-  butSliderR = createButton('R');
-  sliderR = createSlider(0,255,100,1);
-  
+  // butSliderR = createButton('R');
+  sliderR = document.getElementById('sliderR');
+
   // Escala de color en G
-  butSliderG = createButton('G');
-  sliderG = createSlider(0,255,100,1);
+  // butSliderG = createButton('G');
+  sliderG = document.getElementById('sliderG');
 
   // Escala de color en B
-  butSliderB = createButton('B');
-  sliderB = createSlider(0,255,100,1);
+  // butSliderB = createButton('B');
+  sliderB = document.getElementById('sliderB');
+
+  // Escala de color en alpha
+  // butSliderAlpha = createButton('alpha');
+  sliderAlpha = document.getElementById('sliderA');
+
+
+  // // Escala de número de simetrías
+  // butSliderS = createButton('S');
+  // sliderS = createSlider(0, 30, 12, 3);
 }
 
 // Función para guardar el archivo
@@ -74,19 +86,20 @@ function doCirculos(x, y, px, py) {
   ellipse(x, y, speed, speed);
 }
 
-function setCirculos(){
-  circulo=true;
+function setCirculos() {
+  circulo = true;
 }
 
-function resetCirculos(){
-  circulo=false;
+function resetCirculos() {
+  circulo = false;
 }
 
 function draw() {
-   
+
   // Llame al método circulos() y envíele los parámetros
   // para la posición actual del mouse y la posición anterior del mouse
-   //setCirculos(mouseX, mouseY, pmouseX, pmouseY);
+  //setCirculos(mouseX, mouseY, pmouseX, pmouseY);
+  console.log(sliderAlpha.value);
 
   translate(width / 2, height / 2);
 
@@ -99,34 +112,35 @@ function draw() {
     if (mouseIsPressed) {
       for (let i = 0; i < symmetry; i++) {
         rotate(angle);
-        let sw = sizeSlider.value();
-        let r = sliderR.value();
-        let g = sliderG.value();
-        let b = sliderB.value();
+        let sw = sizeSlider.value;
+        let r = sliderR.value;
+        let g = sliderG.value;
+        let b = sliderB.value;
+        let a = sliderAlpha.value;
+        let colores="rgba("+r+","+g+","+b+","+a+")";
+        let c= color(colores);
         strokeWeight(sw);
-        stroke(r,g,b);
-        if(circulo){
+        stroke(c);
+        if (circulo) {
           push();
-          fill(r,g,b);
+          fill(r, g, b);
           doCirculos(mx, my, pmx, pmy);
           pop();
-        }
-        else{
+        } else {
           line(mx, my, pmx, pmy);
         }
-        
 
-        
+
+
         push();
         scale(1, -1);
-        if(circulo){
+        if (circulo) {
           push();
           noStroke();
-          fill(r,g,b);
+          fill(r, g, b);
           doCirculos(mx, my, pmx, pmy);
           pop();
-        }
-        else{
+        } else {
           line(mx, my, pmx, pmy);
         }
         pop();
